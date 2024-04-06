af1a4911b58bd1edfd24031d1284fb4d511aa5ce
UNWHO <eunhoo0713@gmail.com>
2024-04-06 19:24:28 +0900

    refactor: Use vue router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,11 @@
 import { defineComponent } from 'vue'
-import image from './assets/vue.svg'
-import { HelloWorld } from './components/HelloWorld'
+import { RouterView } from 'vue-router'
 
 export default defineComponent({
   name: 'App',
-  setup () {
+  setup() {
     return () => {
-      return [
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo" />
-          </a>
-          <a href="https://vuejs.org/" target="_blank">
-            <img src={image} class="logo vue" alt="Vue logo" />
-          </a>
-        </div>,
-        <HelloWorld msg="Vite + Vue" />
-      ]
+      return <RouterView />
     }
   }
 })
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.tsx'
-import './style.css'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.tsx";
+import { HelloWorld } from "./components/HelloWorld.tsx";
+import "./style.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [{ path: "/", component: HelloWorld }],
+});
+
+app.use(router);
+app.mount("#app");
