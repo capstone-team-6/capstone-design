182817ba13cd944434a65ef48329360c3e93f8ad
UNWHO <eunhoo0713@gmail.com>
2024-05-02 23:45:36 +0900

    feat: Add global auth initialization blocker
    Block app until initial auth state is settled

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,22 @@
-import { defineComponent } from 'vue'
-import { RouterView } from 'vue-router'
+import { getAuth } from "firebase/auth";
+import { defineComponent, reactive } from "vue";
+import { RouterView } from "vue-router";
+import { firebaseApp } from "./utils/firebase";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   setup() {
+    const auth = getAuth(firebaseApp);
+    const state = reactive({
+      isAuthStateReady: false,
+    });
+
+    auth.authStateReady().then(() => (state.isAuthStateReady = true));
+
     return () => {
-      return <RouterView />
-    }
-  }
-})
+      if (!state.isAuthStateReady) return <div>로그인 정보 확인 중</div>;
+
+      return <RouterView />;
+    };
+  },
+});
