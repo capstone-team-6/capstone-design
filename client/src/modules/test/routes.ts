e847b03edecc409c3db99f1f8005d5f55b28ebbe
UNWHO <eunhoo0713@gmail.com>
2024-05-13 14:37:34 +0900

    chore: Delete QR test page

--- client/src/modules/test/routes.ts
+++ client/src/modules/test/routes.ts
@@ -1,6 +1,5 @@
 import { RouteRecordRaw } from "vue-router";
 import Map from "./pages/mapTest";
-import QR from "./pages/qrTest";
 
 export enum TestRoute {
   MAP_TEST = "mapTest",
@@ -15,11 +14,6 @@ export const testRoutes: RouteRecordRaw[] = [
         path: "map",
         component: Map,
         name: TestRoute.MAP_TEST,
-      },
-      {
-        path: "qr",
-        component: QR,
-        name: TestRoute.QR_TEST,
       },
     ],
   },
