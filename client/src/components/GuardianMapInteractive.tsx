34a22cc4612c7f9b3a7d62d33e224dbd1d27ac2b
min-jp <mincjiip@gmail.com>
2024-05-23 23:47:45 +0900

    feat: Add loading functionality to GuardianMapInteractive / fix marker display

--- client/src/components/GuardianMapInteractive.tsx
+++ client/src/components/GuardianMapInteractive.tsx
@@ -7,11 +7,13 @@ import {
   computed,
   defineComponent,
   onMounted,
+  reactive,
   ref,
   watch,
 } from "vue";
 import { Building } from "~/entities/map";
 import MapComponet from "./MapView";
+import Spinner from "./Spinner";
 import UserMarker from "./UserMarker";
 
 export default defineComponent({
@@ -26,11 +28,11 @@ export default defineComponent({
       required: true,
     },
     childs: {
-      type: Object as PropType<{ childName: string; childMarkerId: string }[]>,
+      type: Object as PropType<{ name: string; markerId: string }[]>,
       required: true,
     },
     target: {
-      type: Object as PropType<{ childName: string; childMarkerId: string }>,
+      type: Object as PropType<{ name: string; markerId: string }>,
       required: false,
     },
   },
@@ -43,16 +45,22 @@ export default defineComponent({
       )
     );
 
-    const groupChild = computed(() => _.groupBy(props.childs, "childMarkerId"));
+    const groupChild = computed(() => _.groupBy(props.childs, "markerId"));
 
     const canvasRef = ref<HTMLCanvasElement | null>(null);
+
+    const state = reactive({
+      isLoading: false,
+    });
+
+    let isOverlap: boolean;
 
     const drawCanvas = () => {
       if (canvasRef.value && props.target) {
         const path = useFindPath(
           props.building,
           props.myMarkerId,
-          props.target.childMarkerId
+          props.target.markerId
         );
 
         const ctx = canvasRef.value.getContext("2d");
@@ -75,6 +83,10 @@ export default defineComponent({
           }
           ctx.stroke();
         }
+      } else if (canvasRef.value && !props.target) {
+        const ctx = canvasRef.value.getContext("2d");
+        ctx &&
+          ctx.clearRect(0, 0, canvasRef.value.width, canvasRef.value.height);
       }
     };
 
@@ -92,7 +104,11 @@ export default defineComponent({
       return { x: marker.xLocation, y: marker.yLocation };
     };
 
-    const myPosition = findMarkerPosition(props.myMarkerId);
+    const myPosition = computed(() => findMarkerPosition(props.myMarkerId));
+
+    const mapImgLoading = (isLoading: boolean) => {
+      state.isLoading = isLoading;
+    };
 
     onMounted(() => {
       drawCanvas();
@@ -100,36 +116,57 @@ export default defineComponent({
 
     watch(
       () => props.target,
-      () => drawCanvas()
+      () => {
+        drawCanvas();
+      }
     );
 
     return () => (
-      <MapComponet
-        mapImageURL={floorRef.value ? floorRef.value.mapImageURL : ""}
-        startPosition={findMarkerPosition(props.myMarkerId)}
-      >
-        <canvas ref={canvasRef} width={2600} height={860}></canvas>
-        {_.keys(groupChild.value).map((markerId) => {
-          const markerPosition = findMarkerPosition(markerId);
+      state.isLoading && <Spinner />,
+      (
+        <MapComponet
+          mapImageURL={floorRef.value ? floorRef.value.mapImageURL : ""}
+          startPosition={findMarkerPosition(props.myMarkerId)}
+          onLoad:img={mapImgLoading}
+        >
+          <canvas ref={canvasRef} width={2600} height={860}></canvas>
+          {_.keys(groupChild.value).map((markerId) => {
+            const markerPosition = findMarkerPosition(markerId);
 
-          return (
-            markerPosition && (
-              <UserMarker
-                imageSrc={blueMarkerImgSrc}
-                usersName={_.map(groupChild.value[markerId], "childName")}
-                position={markerPosition}
-              ></UserMarker>
-            )
-          );
-        })}
-        {myPosition && (
-          <UserMarker
-            imageSrc={redMarkerImgSrc}
-            usersName={["Me!"]}
-            position={myPosition}
-          ></UserMarker>
-        )}
-      </MapComponet>
+            if (
+              markerPosition &&
+              myPosition.value &&
+              markerPosition.x === myPosition.value.x &&
+              markerPosition.y === myPosition.value.y
+            ) {
+              isOverlap = true;
+            } else {
+              isOverlap = false;
+            }
+
+            return (
+              markerPosition && (
+                <UserMarker
+                  imageSrc={blueMarkerImgSrc}
+                  usersName={
+                    isOverlap
+                      ? ["-"].concat(_.map(groupChild.value[markerId], "name"))
+                      : _.map(groupChild.value[markerId], "name")
+                  }
+                  position={markerPosition}
+                ></UserMarker>
+              )
+            );
+          })}
+          {myPosition.value && (
+            <UserMarker
+              imageSrc={redMarkerImgSrc}
+              usersName={["[Me]"]}
+              position={myPosition.value}
+            ></UserMarker>
+          )}
+        </MapComponet>
+      )
     );
   },
 });
--- client/src/components/MapView.tsx
+++ client/src/components/MapView.tsx
@@ -12,67 +12,22 @@ export default defineComponent({
       default: { x: 0, y: 0 },
     },
   },
-  emits: ["event:contextmenu"],
+  emits: ["event:contextmenu", "load:img"],
   setup(props, { slots, emit, expose }) {
     const mapRef = ref<HTMLDivElement | null>(null);
-    const position = ref({ x: 0, y: 0 });
-    let start = props.startPosition;
-    let dragging = false;
-
-    const startDrag = (x: number, y: number) => {
-      dragging = true;
-      start.x = x - position.value.x;
-      start.y = y - position.value.y;
-    };
-
-    const onDrag = (x: number, y: number) => {
-      if (!dragging) return;
-      position.value.x = x - start.x;
-      position.value.y = y - start.y;
-    };
-
-    const endDrag = () => {
-      dragging = false;
-    };
-
-    const onMouseDown = (event: MouseEvent) => {
-      event.preventDefault();
-      if (event.button === 0 && mapRef.value) {
-        startDrag(event.clientX, event.clientY);
-        window.addEventListener("mousemove", onMouseMove);
-        window.addEventListener("mouseup", onMouseUp);
-      }
-    };
-
-    const onMouseMove = (event: MouseEvent) => {
-      onDrag(event.clientX, event.clientY);
-    };
-
-    const onMouseUp = () => {
-      endDrag();
-      if (mapRef.value) {
-        window.removeEventListener("mousemove", onMouseMove);
-        window.removeEventListener("mouseup", onMouseUp);
-      }
-    };
-
-    const onTouchStart = (event: TouchEvent) => {
-      const touch = event.touches[0];
-      startDrag(touch.clientX, touch.clientY);
-    };
-
-    const onTouchMove = (event: TouchEvent) => {
-      const touch = event.touches[0];
-      onDrag(touch.clientX, touch.clientY);
-    };
-
-    const onTouchEnd = () => {
-      endDrag();
-    };
+    const position = ref(props.startPosition);
 
     // onContextmenu 이벤트 상위 컴포넌트로 전달
     const onContextMenu = (event: MouseEvent) => {
       emit("event:contextmenu", event);
+    };
+
+    const onImgLoadStart = () => {
+      emit("load:img", true);
+    };
+
+    const onImgLoad = () => {
+      emit("load:img", false);
     };
 
     // getBoundingClientRect 함수 노출
@@ -90,11 +45,9 @@ export default defineComponent({
           backgroundImage: `url("${props.mapImageURL}")`,
           transform: `translate(${position.value.x}px, ${position.value.y}px)`,
         }}
-        onMousedown={onMouseDown}
-        onTouchstart={onTouchStart}
-        onTouchmove={onTouchMove}
-        onTouchend={onTouchEnd}
         onContextmenu={onContextMenu}
+        onLoadstart={onImgLoadStart}
+        onLoad={onImgLoad}
       >
         {slots.default ? slots.default() : null}
       </div>
